64cca485fa2b36c5cee5c70fe002db5bf1b8693c
annehong <anne.hong@pearson.com>
2018-05-22 23:24:02 -0700

    fixes to eraser function and final submission to udacity

--- js/interaction.js
+++ js/interaction.js
@@ -1,13 +1,13 @@
 let draw = true;
 
-console.log("Hi Anne, this is working");
+console.log("hi");
 // Select color input
 console.log($("#colorPicker").val());
 // Select size input
 console.log($("#inputWidth").val());
 console.log($("#inputHeight").val());
 
-//When the size is submitted by the user, call makeGrid() function
+// When size is submitted by the user, call makeGrid()
 //EVENT HANDLERS
 $("input[type='submit']").on("click",function(event) {
     event.preventDefault();
@@ -16,59 +16,64 @@ $("input[type='submit']").on("click",function(event) {
     const height = $("#inputHeight").val();
     const width = $("#inputWidth").val();
 
-    $("pixelCanvas").empty();
+    $("#pixelCanvas").empty();
     makeGrid(height, width);
 });
 
 function makeGrid(height, width) {
-	
-	//y-axis, for number up to height
-	for (let i = 0; i < height; i++) {
-		
-		//create a row
-		//console.log("row" + i);
-		$("#pixelCanvas").append($("<tr></tr>"));
-		
-		//x-axis, for number up to width
-		for (let j = 0; j < width; j++) {
-		
-		//add a td to row
-		//console.log("td" + j);
-		$("tr").last().append($("<td></td>"));
+
+//for number up to height
+for (let i = 0; i < height; i++) {
+
+    //create a row
+  	console.log("row" + i);
+    // console.log("row"+ i);
+    $("#pixelCanvas").append($("<tr></tr>"));
+
+    //for number up to width
+    for (let j = 0; j < width; j++) {
+
+        //add a td to row
+        console.log("td" + j);
+        $("tr").last().append($("<td></td>"));
     }
+
 }
 
-//color pixel square when clicked function
+//color square when clicked function
 $("#pixelCanvas").on("mousedown mouseover", "td", function(e) {
     if (e.buttons===1){
-		
-		if (draw===true) { //global variable check
-			//change background-color of event target's
-			$(this).css("background-color",$("#colorPicker").val());
-		} else {
-			//console.log("erases!"); //test
-			$(this).css("background-color","");
-		}
-	}
+    
+        if (draw===true) { //global var check
+            //change background color of event target's
+            $(this).css("background-color",$("#colorPicker").val());
+        } else {
+            console.log("this is erasing!");
+            $(this).css("background-color","");
+        }
+    }
 });
 
 
-//Toggle between draw/eras
+//toggle between draw/eraser
 $("input[name='tool']").on("change", function() {
-	
-	//console.log($(this).val());
-	console.log("Before conditional draw value = ", draw);
-	
-	//get value of the "changed" input
-	if ($(this).val() === "draw"){
-		draw = true; //change global variable
-	}
-	else if ($(this).val() === "erase"){
-		draw = false; //change global variable
-	}
-	console.log("After conditional draw value = ", draw);
-	
-});
 
+    // console.log($(this).val());
+    console.log("Before conditional draw value=",draw);
+
+    //get value of "changed" input
+  	console.log($(this).val());
+    if ($(this).val() === "draw"){
+        draw=true; //change global var
+    }
+    else if ($(this).val()==="erase"){
+        draw=false; //change global var
+    }
+
+    console.log("After conditional draw value=",draw);
+});
+    
+
+// Your code goes here!
 console.log(width, height);
 }
